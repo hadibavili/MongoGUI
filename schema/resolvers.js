7df4bcb6cee75a37cc8040b38f06023d9c5e7ae9
hadibavili <bavilihadi@gmail.com>
2021-11-14 15:45:24 +0330

    2

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -2,7 +2,6 @@ const JSONdb = require("simple-json-db");
 const db = new JSONdb("../DATA.json");
 const { exec } = require("child_process");
 
-
 const resolvers = {
    Query: {
       getAllUsers() {
@@ -10,14 +9,15 @@ const resolvers = {
       },
    },
    Mutation: {
-      installDB() {
-         exec(`wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
-         sudo apt-get update`, (error, stdout, stderr) => {
-            return stdout;
-         });
-         
-         
-         
+      async installDB() {
+         console.log("start");
+         await exec(
+            `wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
+         sudo apt-get update`,
+            (error, stdout, stderr) => {
+               return stdout;
+            }
+         );
       },
    },
 };
