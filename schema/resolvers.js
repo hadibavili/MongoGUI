6c5eaa11baeaf203e5d3947d6f571554c949d531
hadibavili <bavilihadi@gmail.com>
2021-11-15 16:00:18 +0330

    stream added

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -12,20 +12,19 @@ const resolvers = {
             var q = exec(`sudo apt-get update`, (error, stdout, stderr) => {
                resolve(stdout);
             });
-
-            q.stdout.on("data", function (data) {
-               console.log("stdout: " + data.toString());
-            });
          });
       },
       installMongo() {
          return new Promise(resolve => {
-            exec(
+            var q = exec(
                `sudo apt-get install -y mongodb-org=5.0.2 mongodb-org-database=5.0.2 mongodb-org-server=5.0.2 mongodb-org-shell=5.0.2 mongodb-org-mongos=5.0.2 mongodb-org-tools=5.0.2`,
                (error, stdout, stderr) => {
                   resolve(stdout);
                }
             );
+            q.stdout.on("data", function (data) {
+               console.log("stdout: " + data.toString());
+            });
          });
       },
    },
@@ -43,12 +42,15 @@ const resolvers = {
       },
       creatListFile() {
          return new Promise(resolve => {
-            exec(
+            var q = exec(
                `echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-5.0.list`,
                (error, stdout, stderr) => {
                   resolve(stdout);
                }
             );
+            q.stdout.on("data", function (data) {
+               console.log("stdout: " + data.toString());
+            });
          });
       },
    },
